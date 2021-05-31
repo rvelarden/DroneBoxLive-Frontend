c12ca2adcbb44fbfee5be013613a36790c813758
github username <github email address>
2021-05-31 16:46:30 -0400

    added componentDidMount fetch bookings data

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,11 +7,7 @@ class MainContainer extends Component {
 
 
     state = {
-        drones: [],
-        searchText: '',
-        bookings: [],
-        updatedBookings: {},
-        user: null 
+        bookings: []
         
       }
     
@@ -37,52 +33,14 @@ class MainContainer extends Component {
     })
 }
 
-    editBookingForm = (updateBooking)=>{
-      //  console.log(updateBooking)
-      // this.setState({
-      //   updatedBookings: updateBooking
-      // })
-      let bookings = this.state.bookings.filter(eachBooking => eachBooking.id !== updateBooking.id)
-      this.setState({
-        bookings: [...bookings, updateBooking]
-      })
-    }
-
-    searchBar = (textT)=>{
-      // console.log(textT)
-      this.setState({
-        searchText: textT
-      })
-    }
-
-    addNewBooking = (newBooking)=>{
-      this.setState({
-        bookings: [...this.state.bookings, newBooking]
-      })
-    }
-
-    deleteBooking = (booking) =>{
-      // console.log(artist)
-      fetch('http://localhost:3000/bookings/'+booking.id, {method: 'DELETE'})
-      .then(r => r.json())
-      .then(()=>{
-        this.setState({
-          bookings: this.state.bookings.filter(deletedBooking => deletedBooking.id !== booking.id)
-        })
-      })
-    }
-  
-  
     render(){
-      const filteredDrones = this.state.drones.filter(filterDrone => filterDrone.category.toLowerCase().includes(this.state.searchText))
+   
     return (
-      this.props.currentUser ?
+      
       <div>
-       <SearchBar searchBar={this.searchBar} searchText={this.state.searchText}/>
-        <DroneList drones={filteredDrones} bookings={this.state.bookings} addNewBooking={this.addNewBooking}/>
         <BookingList bookings={this.state.bookings} editBookingForm={this.editBookingForm} drones={this.state.drones} updatedBookings={this.state.updatedBookings} deleteBooking={this.deleteBooking}
         />
-      </div> : null 
+      </div> 
       ) 
     }
   }
